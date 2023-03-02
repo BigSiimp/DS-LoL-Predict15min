43ace10394733e3adf99e9fc47a64d36e6c96cbc
BigSiimp <lukas.f.bade@gmail.com>
2023-03-02 14:05:56 +0100

    work1

--- NNC-predict15.py
+++ NNC-predict15.py
@@ -9,11 +9,11 @@ import random
 data = pd.read_csv('MatchTimelinesFirst15.csv')
 
 # Split data into input (X) and output (Y) variables
-X = data.drop(['blueWins', 'gameId'], axis=1).values
-Y = data['blueWins'].values
+X = data.drop(['blue_win', 'matchId'], axis=1).values
+Y = data['blue_win'].values
 
 # Apply PCA to reduce the number of features to 30
-pca = PCA(n_components=30)
+pca = PCA(n_components=17)
 X = pca.fit_transform(X)
 
 # Scale the input variables
@@ -21,7 +21,15 @@ scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
 # Split the data into training and testing sets
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
+X_test, X_val, Y_test, Y_val = train_test_split(X_test, Y_test, test_size=0.33, random_state=0)
+
+# Shape of the used data
+print(data.shape)
+print(X_train.shape)
+print(X_val.shape)
+print(X_test.shape)
+
 
 # Define the neural network model
 model = MLPClassifier(hidden_layer_sizes=(64, 32), activation='relu', solver='adam', max_iter=1000)
@@ -34,10 +42,10 @@ accuracy = model.score(X_test, Y_test)
 print('Accuracy:', accuracy)
 
 # Choose a random game ID from the dataset
-random_game_id = random.choice(data['gameId'])
+random_game_id = random.choice(data['matchId'])
 
 # Retrieve the corresponding row from the dataset
-new_data = data.loc[data['gameId'] == random_game_id].drop(['blueWins', 'gameId'], axis=1).values
+new_data = data.loc[data['matchId'] == random_game_id].drop(['blue_win', 'matchId'], axis=1).values
 
 # Apply PCA and scaling to the new data
 new_data = pca.transform(new_data)
